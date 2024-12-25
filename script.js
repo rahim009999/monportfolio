166f8c3a4cb23a9e2288ca7a2b47c5ec365fb0e0
rahim009999 <rahimsekkai2024@gmail.com>
2024-12-25 17:56:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Le site Freelance Design est prêt!');
+    // Script moderne sans contact : on peut ajouter des animations ou d'autres actions ici
+});
